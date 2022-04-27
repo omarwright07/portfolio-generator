d2fd62de92de1d388f8bafad01555ea915684b5f
Omar Wright <98981985+omarwright07@users.noreply.github.com>
2022-04-27 17:23:58 -0500

    input question answers into the html template

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', generatePage(name, github), err => {
-//     if (err) throw new Error(err);
-
-//     console.log('Portfilo complete! Check out index.html to see the output?');
-// });
+const generatePage = require('./src/page-template.js');
+const fs = require('fs');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -134,5 +126,12 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(projectData => {
-        console.log(projectData);
+        const pageHTML = generatePage(projectData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('Portfilo complete! Check out index.html to see the output?');
+        });
+
     });
